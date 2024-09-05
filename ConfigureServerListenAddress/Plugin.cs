99f9847d166ec8afed3057e4decbe85ca51c42b0
vibrant-reindeer <159484964+vibrant-reindeer@users.noreply.github.com>
2024-09-05 12:02:13 +0200

    INITIAL IMPORT. Empty mod, doesn't do anything yet.

---
+++ ConfigureServerListenAddress/Plugin.cs
@@ -0,0 +1,47 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using SpaceCraft;
+using UnityEngine;
+using Unity.Netcode;
+using Unity.Netcode.Transports;
+using Unity.Netcode.Transports.UTP;
+using System.ComponentModel;
+using Netcode.Transports;
+using System.Text;
+using System.IO;
+using System.Collections;
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ConfigureServerListenAddress
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private static ManualLogSource logger;
+        private static ConfigEntry<string> serverListenAddress;
+
+        private void Awake()
+        {
+            Plugin.logger = Logger;
+            serverListenAddress = Config.Bind("General", "ServerListenAddress", "::", "The server listen address. Default is '::' to bind to all IPv6 addresses.)");
+
+            Harmony harmony = new($"{PluginInfo.PLUGIN_GUID}.{PluginInfo.PLUGIN_NAME}.{PluginInfo.PLUGIN_VERSION}");
+            harmony.Patch(
+                original: AccessTools.Method("Unity.Netcode.NetworkManager:StartHost"),
+                prefix: new HarmonyMethod(typeof(Plugin), nameof(StartHost_Prefix))
+            );
+
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+
+        public static void StartHost_Prefix()
+        {
+
+        }
+    }
+}
